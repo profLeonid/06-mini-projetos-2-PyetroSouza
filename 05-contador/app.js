3490865c8af4c80a950c70dd7544dbfeb0691c1b
PYETRO FERREIRA DE SOUZA <25205137@cfp127aluno.local>
2026-03-23 16:56:27 -0300

    feat: adicionar funcoes

---
+++ 05-contador/app.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const quantidade = document.getElementById('quantidade')
+
+function criarListaNumeros(quantidade){
+    let listaNumeros = []
+
+    for (let i = 1; i <= quantidade; i++){
+        listaNumeros.push(i)
+    }
+    return listaNumeros
+}
+
+// console.log(criarListaNumeros(6))
+function criarListaNumerosPares(quantidade){
+    let listaPares = []
+
+    for(let i = 0; i <= quantidade; i++){
+        if (i % 2 === 0)
+        listaPares.push(i)
+    }
+    return listaPares
+}
+function criarListaNumerosImpares(quantidade){
+    let listaImpares = []
+
+    for(let i = 0; i <= quantidade; i++){
+        if (i % 2 !== 0)
+        listaImpares.push(i)
+    }
+    return listaImpares
+}
+function criarListaMultiploCinco(quantidade){
+    let listaMultiplosCinco = []
+    for (let i = 1; i <= quantidade; i++){
+        if (i % 5 === 0)
+            listaMultiplosCinco.push(i)
+    }
+    return listaMultiplosCinco
+}
+function criarListaNumerosAoQuadrado(quantidade){
+    let listaNumerosAoQuadrado = []
+
+    for(let i = 0; i <= quantidade; i++){
+        listaNumerosAoQuadrado.push(Math.pow(2, i));
+    }
+    return listaNumerosAoQuadrado
+}
+function criarLinha(numero, numeroPar, numeroImpar, numeroMulti, numeroPotencial){
+    
+}
+
+
+function gerarLista(){
+    console.log(criarListaMultiploCinco(quantidade))
+}
+
+// console.log(criarListaMultiploCinco(10))
+// console.log(criarListaNumerosPares(10))
+// console.log(criarListaNumerosImpares(10))
+console.log(criarListaNumerosAoQuadrado(10))
